56c97ade8518f0ba1b49d353bb0b0b9f08db1653
SriMoorthy <srimoorthy27@gmail.com>
2024-07-29 17:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 //your JS code here. If required.
-function resize() {
-	let screenSize = document.getElementById('sizeInfo');
-    screenSize.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
-}
-
-window.addEventListener('resize',resize);
-
-resize();
