d40d1886c66694e2c7579e3aa7f0458e8270ba60
SriMoorthy <srimoorthy27@gmail.com>
2024-07-29 16:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function load() {
+	let screenSize = document.getElementById('sizeInfo');
+    screenSize.innerHTML = `Width: ${screen.width} and Height: ${screen.height}`
+}
